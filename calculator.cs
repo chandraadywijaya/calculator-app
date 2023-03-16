e730d35eb91f05fe694648e1755419921d92d08e
chandraadywijaya <chandraadywijaya@students.amikom.ac.id>
2023-03-16 14:56:23 +0700

    menambahkan fungsi pengurangan

--- calculator.cs
+++ calculator.cs
@@ -5,6 +5,9 @@ class Calculator{
 		
 		console.WriteLine("Hasil Penambahan :{0} + {1} = {2}", a, b, penambahan(a, b));
 		console.WriteLine("Hasil Pengurangan :{0} - {1} = {2}", a, b, pengurangan(a, b));
+		console.WriteLine("Hasil Perkalian :{0} * {1} = {2}", a, b, perkalian(a, b));
+		console.WriteLine("Hasil Pembagian :{0} \ {1} = {2}", a, b, pembagian(a, b));
+		
 		
 		console.WriteLine("\nTekan sembarang key untuk keluar");
 		console.ReadyKey();
